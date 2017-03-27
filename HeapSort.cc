a1611764999a11f26eb19d23102ac2a5361574b2
tim <tim@Narwhal.stny.rr.com>
2017-03-27 14:38:45 -0400

    Implement HeapSort and General Heap template The HeapSort function and the associated examples explains why make heap using less<> create a max-heap. all examples use max-heap, since if not specified, less<T> will be used as the comparator. Functions implemented: Parent(begin, i) Left(begin,i) Right(begin,i) HeapifyUp(begin,i,comparator) HeapifyDown(begin,end,i,comparator) BuildHeap(begin,end,comparator) SortHeap(begin,end,comparator) //the input must be a heap HeapSort(begin,end,comparator) //wrapped BuildHeap and SortHeap HeapPush(begin,end,comparator) //the last elem is the key to be pushed HeapPop(begin,end,comparator) //Heap top be popped and placed at the end of the array

--- HeapSort.cc
+++ HeapSort.cc
@@ -1,6 +1,5 @@
 /*
  * Worst-case running time O(n lg n)
- *
  * min-heap property:	A[PARENT(i)] ≤ A[i]
  * max-heap property: A[PARENT(i)] ≥ A[i]
  * PARENT(i)
@@ -37,4 +36,181 @@
  * 		exchange A[1] with A[i]
  * 		A.heap-size = A.heap-size - 1
  * 		MAX-HEAPIFY(A,1)
+ *
+ * HEAP-MAXIMUM(A)
+ * 	return A[1]
+ *
+ * HEAP-EXTRACT-MAX(A)
+ * 	if A.heap-size < 1
+ * 		error "heap underflow"
+ * 	max = A[1]
+ * 	A[1] = A[A.heap-size - 1]
+ * 	MAX-HEAPIFY(A,1)
+ * 	return max
+ *
+ * HEAP-INCREASE-KEY(A,i,key)
+ * 	if key < A[i]
+ * 		error "new key is smaller than current key"
+ * 	A[i] = key
+ * 	while i > 1 and A[PARENT(i)] < A[i]
+ * 		exchange A[i] with A[PARENT(i)]
+ * 		i = PARENT(i)
+ *
+ * MAX-HEAP-INSERT(A,key)
+ * 	A.heap-size = A.heap-size + 1
+ * 	A[A.heap-size] = -∞
+ * 	HEAP-INCREASE-KEY(A,A.heap-size,key)
+ *
  */
+
+#include <iterator>
+#include <functional>
+
+#define DEBUG
+#ifdef DEBUG
+#include <vector>
+#include <string>
+#include <cstdio>
+#endif
+template <typename Itr>
+auto Parent(Itr p, Itr i) {
+	return std::next(p,std::distance(p,std::prev(i))>>1);
+}
+template <typename Itr>
+auto Left (Itr p, Itr i) {
+	return std::next(p,(std::distance(p,i)<<1) + 1);
+}
+template <typename Itr>
+auto Right (Itr p, Itr i) {
+	return std::next(p,(std::distance(p,i)<<1) + 2);
+}
+
+template <typename Itr, typename Cmp>
+void HeapifyUp(Itr p, Itr i, Cmp cmp) {
+	while (i > p && cmp(*Parent(p,i),*i)) {
+		std::iter_swap(Parent(p,i),i);
+		i = Parent(p,i);
+	}
+}
+
+template <typename Itr, typename Cmp>
+void HeapifyDown(Itr p, Itr q, Itr i, Cmp cmp) {
+	auto l = Left(p,i);
+	auto r = Right(p,i);
+	auto t = i;
+	if (l < q && cmp(*i,*l)) {
+		t = l;
+	}
+	if (r < q && cmp(*t,*r)) {
+		t = r;
+	}
+	if (t != i) {
+		std::iter_swap(t,i);
+		HeapifyDown(p,q,t,cmp);
+	}
+}
+
+template <typename Itr, typename Cmp>
+void BuildHeap(Itr p, Itr q, Cmp cmp) {
+	if (std::distance(p,q) < 2) return;
+	for (auto i = Parent(p,std::prev(q)); i >= p; --i) {
+		HeapifyDown(p,q,i,cmp);
+	}
+}
+
+template <typename Itr, typename Cmp>
+void SortHeap(Itr p, Itr q, Cmp cmp) {
+	if (std::distance(p,q) < 2) return;
+	for (auto i = std::prev(q); i > p; --i) {
+		std::iter_swap(i,p);
+		HeapifyDown(p,i,p,cmp);
+	}
+}
+
+template <typename Itr, typename Cmp>
+void HeapSort(Itr p, Itr q, Cmp cmp) {
+	BuildHeap(p,q,cmp);
+	SortHeap(p,q,cmp);
+}
+template <typename Itr>
+void HeapSort(Itr p, Itr q) {
+	HeapSort(p,q,std::less<typename std::iterator_traits<Itr>::value_type>());
+}
+
+template <typename Itr>
+auto HeapTop(Itr p) {
+	return *p;
+}
+
+template <typename Itr, typename Cmp>
+void HeapPop(Itr p, Itr q, Cmp cmp) {
+	if (p == q) return;
+	--q;
+	std::iter_swap(p,q);
+	HeapifyDown(p,q,p,cmp);
+}
+
+template <typename Itr, typename Cmp>
+void HeapPush(Itr p, Itr q, Cmp cmp) {
+	--q;
+	HeapifyUp(p,q,cmp);
+}
+
+//test
+#ifdef DEBUG
+template <typename It>
+void printIA(It b, It e) {
+	for (;b != e; ++b) fprintf(stderr,"%-4d",*b);
+	fprintf(stderr,"\n");
+}
+template <typename It>
+void printS(It b, It e) {
+	for (;b != e; ++b) fprintf(stderr,"%c",*b);
+	fprintf(stderr,"\n");
+}
+
+void testHeap() {
+	int a1[] = {1,6,2,9,4,3,7,5};
+	std::vector<int> h;
+	printf("Heap Push\n");
+	for (auto x : a1) {
+		h.push_back(x);
+		printf("%-4d",x);
+		HeapPush(std::begin(h),std::end(h),std::less<int>());
+	}
+	printf("\nHeap Pop\n");
+	while (!h.empty()) {
+		HeapPop(std::begin(h),std::end(h),std::less<int>());
+		printf("%-4d",h.back());
+		h.pop_back();
+	}
+	printf("\n");
+}
+#endif
+using namespace std;
+int main() {
+#ifdef DEBUG
+	int a1[] = {7,6,5,4,3,2,1,0,9,8};
+	int a2[] = {0,0,0,0,0,0,0};
+	int a3[] = {0,0,0,0,0,0,0,1,1,1,1,2,2,2,2};
+	int a5[] = {1};
+	std::vector<int> a6 = {7,6,5,4,3,2,1};
+	std::vector<int> a7 = {};
+	std::string s1{"What a wonderful day!"};
+	HeapSort(begin(a1),end(a1));
+	HeapSort(begin(a2),end(a2));
+	HeapSort(begin(a3),end(a3));
+	HeapSort(begin(a5),end(a5));
+	HeapSort(begin(a6),end(a6));
+	HeapSort(begin(s1),end(s1));
+	HeapSort(begin(a7),end(a7));
+	printIA(begin(a1),end(a1));
+	printIA(begin(a2),end(a2));
+	printIA(begin(a3),end(a3));
+	printIA(begin(a5),end(a5));
+	printIA(begin(a6),end(a6));
+	printS(begin(s1),end(s1));
+	printIA(begin(a7),end(a7));
+	testHeap();
+#endif
+}
